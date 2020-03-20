8b4fe6b55e7e60f2f22a622971a9d395de2a33ce
Salvador <salvador.on@outlook.com>
2020-03-20 18:38:08 -0300

    add 2 rows frienship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_220558) do
+ActiveRecord::Schema.define(version: 2020_03_20_213005) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_03_19_220558) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["friend_id"], name: "index_friendships_on_friend_id"
+    t.index ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id", unique: true
     t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
